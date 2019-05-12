1ef75857caba4d71a9c8dc384285fd6e5f0b5a89
Ming Hao Chan <cmh114933@gmail.com>
2019-05-13 01:13:50 +0800

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatInputComponent } from './chat-input/chat-input.component';
+import { ChatContactComponent } from './chat-contact/chat-contact.component';
+import { ChatMessagesComponent } from './chat-messages/chat-messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatInputComponent,
+    ChatContactComponent,
+    ChatMessagesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/chat-contact/chat-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-contact',
+  templateUrl: './chat-contact.component.html',
+  styleUrls: ['./chat-contact.component.css']
+})
+export class ChatContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
